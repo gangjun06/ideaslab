5966fb498befa478d02b5b5bf5fc67a5a5fd64ee
이강준 <me@gangjun.dev>
2022-12-13 19:49:38 +0900

    chore: setup monorepo

---
+++ packages/eslint-config/eslint-preset.js
@@ -0,0 +1,66 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "prettier",
+    "unused-imports",
+    "simple-import-sort",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "react/display-name": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          // Packages `react` related packages
+          ["^react", "^next", "^@(?!ideaslab).+"],
+          // Monorepo packages
+          ["^@ideaslab(/.*|$)"],
+          // Internal packages.
+          ["^~(/.*|$)"],
+          // Relative imports
+          [
+            "^\\.\\.(?!/?$)",
+            "^\\.\\./?$",
+            "^\\./(?=.*/)(?!/?$)",
+            "^\\.(?!/?$)",
+            "^\\./?$",
+          ],
+          // Style imports.
+          ["^.+\\.?(css|scss)$"],
+        ],
+      },
+    ],
+  },
+};
