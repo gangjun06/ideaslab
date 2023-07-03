f0bef342cafb8991c3be0db0b4926359a331af8b
Kangjun Lee <me@gangjun.dev>
2023-07-03 23:04:58 +0900

    feat: 점수판

--- apps/server/src/bot/commands/util/point.ts
+++ apps/server/src/bot/commands/util/point.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { APIEmbedField, SlashCommandBuilder } from 'discord.js'
 
 import { SlashCommand } from '~/bot/base/command'
 import { pointService } from '~/service/point'
@@ -9,10 +9,19 @@ export default new SlashCommand(
     .setName('이벤트')
     .setDescription('얻은 포인트를 확인해요.')
     .addSubcommand((command) => command.setName('포인트').setDescription('내 포인트를 확인해요.'))
+    .addSubcommand((command) =>
+      command.setName('점수판').setDescription('이벤트 점수판을 확인해요.'),
+    )
     .addSubcommand((command) => command.setName('랭킹').setDescription('이벤트 랭킹을 확인해요.')),
   async (client, interaction) => {
     const subcommand = interaction.options.getSubcommand()
-    if (subcommand === '포인트') {
+    if (subcommand === '점수판') {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          'https://media.discordapp.net/attachments/765200673423491072/1125426753624604692/image.png?width=921&height=809',
+      })
+    } else if (subcommand === '포인트') {
       const { user } = interaction
 
       const point = (await pointService.get(user.id))._sum.value ?? 0
@@ -24,9 +33,10 @@ export default new SlashCommand(
 
       interaction.reply({ embeds: [embed], ephemeral: true })
     } else if (subcommand === '랭킹') {
-      const ranking = (await pointService.rank()).map((item, index) => ({
+      const ranking: APIEmbedField[] = (await pointService.rank()).map((item, index) => ({
         name: `${index + 1}위`,
         value: `<@${item.userId}> - ${item.value}점`,
+        inline: true,
       }))
       const embed = new Embed(client, 'info')
         .setTitle(`이벤트 랭킹`)
